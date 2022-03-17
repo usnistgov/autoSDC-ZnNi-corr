d421ca420c0f7401059ca970fbe10ff6262957c8
Brian DeCost <brian.decost@nist.gov>
2022-03-17 16:34:53 -0400

    report generating script.

---
+++ src/generate_report.py
@@ -0,0 +1,40 @@
+"""Process and analyze all raw electrochemical data into results table."""
+import os
+import pandas as pd
+from pathlib import Path
+
+from asdc.analysis.report import process_row
+
+# session keys and sample ids to exclude from analysis
+# (for insufficient data quality)
+drop_ids = {
+    "zinni-2020-10-16": [15, 18],
+    "zinni-2020-10-26": [1],
+    "zinni2-2020-11-24": [28, 34, 38],
+    "zinni3_1-2020-12-21": [],
+}
+
+
+def load_session(x):
+    print(x)
+    return "x"
+
+
+if __name__ == "__main__":
+    data_dir = Path("data")
+    data_dir.mkdirs(exists_ok=True)
+
+    dataframes = []
+    for idx, (run, drop) in enumerate(drop_ids.items()):
+        df = load_session(str(data_dir / run / "data/sdc.db"))
+        df = df[~df.id.isin(drop)]
+        df["session"] = run
+        df["experiment"] = f"dat{idx+1}"
+        dataframes.append(df)
+
+    df = pd.concat(dataframes)
+
+    df.sort_values(by="E_oc_stable", inplace=True)
+    df.dropna(inplace=True, subset=["pH_med"])
+    df.reset_index(inplace=True)
+    df.to_json(data_dir / "fulldataset.json")
